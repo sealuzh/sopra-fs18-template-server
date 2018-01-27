bd90f29a036b58b85f6c0629cd36446e0ee4882e
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-01-27 19:00:52 +0100

    UserService and UserResource refactored

--- src/main/java/ch/uzh/ifi/seal/soprafs17/service/UserService.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs17/service/UserService.java
@@ -10,10 +10,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 
 /**
+ * /**
+ * Created by LucasPelloni on 26.01.18.
  * Service class for managing users.
  */
 @Service
@@ -21,6 +25,7 @@ import java.util.List;
 public class UserService {
 
     private final Logger log = LoggerFactory.getLogger(UserService.class);
+
 
     private final UserRepository userRepository;
 
@@ -31,9 +36,7 @@ public class UserService {
     }
 
 
-
     public User createUser(String name, String username, String token, UserStatus status, List<Game> games) {
-
         User newUser = new User();
         newUser.setName(name);
         newUser.setUsername(username);
@@ -45,13 +48,43 @@ public class UserService {
         return newUser;
     }
 
+    public User login(Long userId) {
+        User user = userRepository.findOne(userId);
+        if (user != null) {
+            user.setToken(UUID.randomUUID().toString());
+            user.setStatus(UserStatus.ONLINE);
+            user = userRepository.save(user);
+            return user;
+        }
+        return null;
+    }
+
+    public void logout(Long userId, String userToken) {
+        User user = userRepository.findOne(userId);
+        if (user != null && user.getToken().equals(userToken)) {
+            user.setStatus(UserStatus.OFFLINE);
+            userRepository.save(user);
+        }
+    }
+
+    public User addUser(User user) {
+        user.setStatus(UserStatus.OFFLINE);
+        user.setToken(UUID.randomUUID().toString());
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        List<User> result = new ArrayList<>();
+        userRepository.findAll().forEach(result::add);
+        return result;
+    }
+
 
     public void deleteUser(Long id) {
         User user = userRepository.findById(id); //TODO check if user exists
         userRepository.delete(id);
         log.debug("Deleted User: {}", user);
     }
-
 
 
 }
